a33aeb4aba6b1f67ca03a4d7a1429ff2e1042657
Chelsea Mutizira <chelseamutizira@gmail.com>
2020-07-09 16:17:48 -0700

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,4 @@
-<?php include('config/
-server.php') ?>
+<?php include('config/server.php') ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -39,48 +38,4 @@ server.php') ?>
 </html>
 
 
-<?php
-session_start();
 
-if(isset($_SESSION['username'])) {
-    $_SESSION['msg'] = "You must log in first";
-    header('location: login.php');
-}
-
-if(isset($_GET['logout'])){
-    session_destroy();
-    unset($_SESSION['username']);
-    header('location:login.php');
-}
-
-?>
-
-
-<div class="header">
-  <h1>Home Page</h1>
- <?php include ("register.php") ?>
-</div>
-
-<div class="content">
-<!--notification message-->
-<?php if(isset($_SESSION['success'])) : ?>
-<div class="error success">
- <h3>
- <?php
- echo $_SESSION['success'];
- unset($_SESSION['success']);
- ?>
- </h3>
-</div>
-<?php endif ?>
-
- <!--logged in user information -->
- <?php if(isset($_SESSION['username'])) : ?>
- <p>Welcome <strong><?php echo $_SESSION['username']; ?></p>
- <p> <a href= "index.php?logout='1'" style= "color: red;"></p>
- 
- <?php endif ?>
-</div>
-
-</body>
-</html>
